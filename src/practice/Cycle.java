6ef7f80fe121f1563d2ce4edc6ea84c283cf6d87
3248854792 <3248854792@qq.com>
2021-04-30 22:10:49 +0800

    Initial commit

--- src/practice/Cycle.java
+++ src/practice/Cycle.java
@@ -4,10 +4,16 @@ public class Cycle {
     public void play() {
         System.out.println("Cycle");
     }
+    public void wheels(){
+        System.out.println("轮子的数量是"+"num");
+    }
 }
 class Unicycle extends Cycle {
     public void play() {
         System.out.println("Unicycle");
+    }
+    public void wheels(){
+        System.out.println("Unicycle wheels:4");
     }
 }
 
@@ -15,11 +21,17 @@ class Bicycle extends Cycle {
     public void play() {
         System.out.println("Bicycle");
     }
+    public void wheels(){
+        System.out.println("Bicycle wheels:2");
+    }
 }
 
 class Tricycle extends Cycle {
     public void play() {
         System.out.println("Tricycle");
+    }
+    public void wheels(){
+        System.out.println("Tricycle wheels:3");
     }
 }
 class Ride {
@@ -34,5 +46,10 @@ class Ride {
         ride(unicycle);
         ride(bicycle);
         ride(tricycle);
+        unicycle.wheels();
+        bicycle.wheels();
+        tricycle.wheels();
+
     }
 }
+
