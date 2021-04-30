d38d0c9d01b77a607df7cca61fdd6e45f7b823aa
3248854792 <3248854792@qq.com>
2021-04-30 21:50:14 +0800

    Initial commit

---
+++ src/practice/Test1.java
@@ -0,0 +1,6 @@
+package practice;
+
+class Test1 {
+    public static abstract void main(String[] args) {
+        new Test1();//error 如下图
+    }}
---
+++ src/practice/Test2.java
@@ -0,0 +1,9 @@
+package practice;
+
+public abstract class Test2 {
+}
+class Test {
+    public static void main(String[] args) {
+        new Test2();//error 如下图
+    }
+}
